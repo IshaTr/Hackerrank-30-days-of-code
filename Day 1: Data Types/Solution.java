c092ee059c84fb47de871a29e1a3f98b0fe30a9e
ishatr <isha212195@gmail.com>
2017-08-31 01:51:30 +0530

    Add day-1 code :fire:

---
+++ Day 1: Data Types/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args){
+        int num = scan.nextInt();
+        double dnum = scan.nextDouble();
+        String str = scan.nextLine();
+        String str2 = scan.nextLine();
+
+        System.out.println(i + num);
+
+        System.out.println(d + dnum);
+        
+        System.out.println(s + str2);
+    }
+}
